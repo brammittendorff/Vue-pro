0da2a1272ef8e1181567eaeaf49955391a3b5a1e
zhangbao <zhangbao@mianchao-inc.com>
2019-05-14 12:01:43 +0800

    showecharts

---
+++ src/module/ShowEcharts.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class='content'>
+<div class='g-container g-box-shadow'>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {},
+
+ mounted() {},
+}
+
+</script>
+<style lang='less' rel='stylesheet/less' scoped>
+</style>
+<style rel='stylesheet/less' lang='less'>
+</style>
