9b12428505f78f5b0fd85edd8b409585e4f8da1e
zhangbao <zhangbao@mianchao-inc.com>
2019-05-15 11:52:37 +0800

    配置文件注释

--- config/index.js
+++ config/index.js
@@ -5,28 +5,28 @@
 const path = require('path')
 
 module.exports = {
-  dev: {
+  dev: { //开发环境配置项
 
     // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {},
+    assetsSubDirectory: 'static', // 虚拟静态资源地址
+    assetsPublicPath: '/', 
+    proxyTable: {}, // 服务器代理
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
-    errorOverlay: true,
-    notifyOnErrors: true,
+    port: 8080, // 本地服务器开启的端口。如果被占用,则会自动开启一个端
+    autoOpenBrowser: false, // 服务开启是否自动打开浏览器
+    errorOverlay: true, // 是否错误叠加
+    notifyOnErrors: true, // 是否通知错误
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
     // Use Eslint Loader?
     // If true, your code will be linted during bundling and
     // linting errors and warnings will be shown in the console.
-    useEslint: true,
-    // If true, eslint errors and warnings will also be shown in the error overlay
-    // in the browser.
-    showEslintErrorsInOverlay: false,
+    useEslint: true, // 是否开启eslint代码检查
+    // if 值为true，eslint显示错误和警告的错误也将被覆盖
+    // 在浏览器中.
+    showEslintErrorsInOverlay: false, 
 
     /**
      * Source Maps
@@ -38,39 +38,39 @@ module.exports = {
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
-    cacheBusting: true,
+    cacheBusting: true, // 是否清除缓存
 
-    cssSourceMap: true
+    cssSourceMap: true // css样式是否有sourcemap
   },
 
-  build: {
+  build: { // 生产环境配置项
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),  // 打包生成模版
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../dist'), // 打包根路径
+    assetsSubDirectory: 'static', // 打包静态资源地址 
+    assetsPublicPath: '/', // 公共路径
 
     /**
      * Source Maps
      */
-
-    productionSourceMap: true,
+    // 能够提供将压缩文件恢复到源文件原始位置的映射代码的方式，这意味着你可以在优化压缩代码后轻松的进行调试
+    productionSourceMap: true, // sourcemap
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
+    // Before setting to `true`, make sure to:  在设置下面为 `true`,请确认执行下面指令
     // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
+    productionGzip: false, // 是否开启 gzip
+    productionGzipExtensions: ['js', 'css'], // 需要使用 gzip 压缩的文件扩展名
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
+    // `npm run build --report`  执行这个命令会开启本地服务器 打开一个报告页面
     // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report 
   }
 }
