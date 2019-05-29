f5465c6818725ddeba3259002e8045a2891d7876
zhangbao <zhangbao@mianchao-inc.com>
2019-05-29 16:12:58 +0800

    手写tabs 增删改查

--- src/module/ShowEcharts.vue
+++ src/module/ShowEcharts.vue
@@ -4,6 +4,14 @@
   <div class='g-container g-box-shadow'>
       <v-echarts id='echarts' width='800px' height='400px' :option='defaultOption' ></v-echarts>
   </div>
+
+  <div>
+    <div style="float:left">
+    <label class="label" v-for="(item,index) in tabdata" :key="index" >{{item.name}}&emsp;<span @click="del(index)">X</span></label>
+    <el-input v-if="showinput" class="label" size="samll" v-model="newname" ref="newn" @blur="blur"></el-input>
+    </div>
+    <el-button style="float:left" @click="add">+新增</el-button>
+  </div>
 </div>
 </template>
 
@@ -14,17 +22,47 @@ import data from "../assets/data";
 export default {
   data() {
     return {
-      defaultOption: {}
+      defaultOption: {},
+      tabdata:[
+        {id:0,name:'tab11'},
+        {id:1,name:'tab22'},
+        {id:2,name:'tab33'},
+        {id:3,name:'tab44'}
+      ],
+      newname:'',
+      showinput:false,
     };
   },
-
   components: {
     vEcharts
   },
-
-  computed: {},
-
   methods: {
+    add(){
+      console.log('add')
+      this.showinput = true
+       this.$nextTick( () =>{
+       this.$refs.newn.focus()
+      } )
+    },
+   
+    del(index){
+      console.log('del')
+      this.tabdata.splice(index,1)
+    },
+    blur(){
+      console.log('blur')
+      if(this.newname){
+          this.tabdata.push(
+              {
+                id:this.tabdata[this.tabdata.length-1].id+1,
+                name:this.newname
+              }
+            )
+      }
+      
+      this.newname = ''
+      this.showinput = false
+    },
     getdata() {
       this.defaultOption = {
         title: {
@@ -135,6 +173,13 @@ export default {
 };
 </script>
 <style lang='less' rel='stylesheet/less' scoped>
+.label{
+  width: 50px;
+  border: 1px solid #ccc;
+  padding: 10px 0px;
+  text-align: center;
+  margin: 0px 10px;
+}
 </style>
 <style rel='stylesheet/less' lang='less'>
 </style>
