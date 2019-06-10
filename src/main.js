18436f16be94f9970cd929acca3aa1720aa5be49
zhangbao <zhangbao@mianchao-inc.com>
2019-06-10 15:20:32 +0800

    全局注册i80

--- src/main.js
+++ src/main.js
@@ -9,6 +9,24 @@ import './assets/less/global.less';
 import './assets/less/lib/index.less';
 import filters from './components/util/filters'
 
+import enLocale from 'element-ui/lib/locale/lang/en'
+import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
+import ElementLocale from 'element-ui/lib/locale'
+import VueI18n  from 'vue-i18n'
+
+Vue.use(VueI18n)
+const messages = {
+  en: {...enLocale},
+  zh: {...zhLocale}
+}
+const i18n = new VueI18n({
+  locale: localStorage.getItem('language')||'en', 
+  messages, 
+})
+
+ElementLocale.i18n((key, value) => i18n.t(key, value))
+
+
 Object.keys(filters).forEach(key => {
   console.log(filters,key,filters[key],'/././.....')
 Vue.filter(key, filters[key])
@@ -30,6 +48,7 @@ new Vue({
   el: '#app',
   store,
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
