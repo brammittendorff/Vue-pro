63a20613a3bdd363543b68fd1167a07103e174f3
babybrotherzb <babybrotherzb@gmail.com>
2019-10-08 18:07:48 +0800

    vuex-demo

--- src/module/vuex_demo/vuex.vue
+++ src/module/vuex_demo/vuex.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="echarts_box">
     <a href="/#/echarts">前往echarts</a>
-    <input  v-model="counts" />
-    <button @click.stop="add" >增加1</button>
-    <button @click.stop="reduce" >减少1</button>
-    <button @click.stop="add1000" >增加1000</button>
-    <button @click.stop="reduce1000" >减少1000</button>
-    <div v-cloak  v-text="name"></div>
-    <div  v-html="html"></div>
+    <input v-model="counts" />
+    <button @click.stop="add">增加1</button>
+    <button @click.stop="reduce">减少1</button>
+    <button @click.stop="add1000">增加1000</button>
+    <button @click.stop="reduce1000">减少1000</button>
+    <div
+      v-cloak
+      v-text="name"
+    ></div>
+    <div v-html="html"></div>
     <div v-pre>v-pre跳过渲染</div>
-    <input v-model.number.trim='number' placeholder="model修饰符" />
+    <input
+      v-model.number.trim='number'
+      placeholder="model修饰符"
+    />
     <!-- <img src="./assets/logo.png"> -->
-      <tree :model="root" :length="length"></tree>
+    <tree
+      :model="root"
+      :length="length"
+    ></tree>
 
   </div>
 </template>
@@ -21,7 +30,7 @@ import { mapState, mapMutations, mapActions, mapGetters } from "vuex";
 import tree from "./tree.vue";
 
 export default {
-  components:{
+  components: {
     tree
   },
   data() {
@@ -29,7 +38,7 @@ export default {
       name: "大家好",
       html: "<h2>才是真的好</h2>",
       number: "",
-      
+
       root: {
         name: "根节点",
         level: 0,
@@ -126,8 +135,8 @@ export default {
 [v-cloak] {
   display: none;
 }
-.echarts_box{
-  height: calc(~'100vh - 60px');
+.echarts_box {
+  height: calc(~"100vh - 60px");
   position: relative;
   top: 60px;
 }
